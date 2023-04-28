72bc93588423c5cabb08990ca8fec84d10167b50
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-04-28 13:15:42 +0300

    HW 2  (CODE STYLE)

--- Banana.java
+++ Banana.java
@@ -1,7 +1,7 @@
 public class Banana extends Fruit{
 
 
-    public Banana(int fruitWeight){
+    public Banana(int fruitWeight){   // HW 2 (CODE STYLE)- i think the name of the variables should be called using camelCase method, in the video variable fruitWeight is written "weight".
         // constructor delegation (Fruit constructor)
         super(fruitWeight);
         usefulVolume = 40; //%
