3a01a885ea6210e2c56bcb9f9cd624f94561431b
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-11 16:10:36 +0300

    Update Fruit.java
    added an abstract method and encapsulation for weigth parameter

--- Fruit.java
+++ Fruit.java
@@ -1,5 +1,16 @@
 abstract public class Fruit{
+    
     public int fruitWeight;
+
+
+    public int getFruitWeight() {
+        return fruitWeight;
+    }
+
+    public void setFruitWeight(int fruitWeight) {
+        this.fruitWeight = fruitWeight;
+    }
+
     int usefulVolume;
 
     public Fruit(){}
@@ -7,4 +18,8 @@ abstract public class Fruit{
     public Fruit(int fruitWeight){
         this.fruitWeight = fruitWeight;
     }
+
+    abstract public void printVitamines();
+        
+    
 }
