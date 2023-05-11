52e847c9f0dce289743f2fa87afcf4d393973808
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-11 16:22:27 +0300

    Update PineApple.java

--- PineApple.java
+++ PineApple.java
@@ -3,8 +3,18 @@ public class PineApple extends Fruit{
     public PineApple(int fruitWeight){
         // constructor delegation (Fruit constructor)
         super(fruitWeight);
-        usefulVolume = 90; //%
+        usefulVolume = 95; //%
        
     }
 
+    public PineApple(){
+        super();
+    }
+
+    public void printVitamines(){
+        System.out.println("Vitamines A, B6, E, and K. Calcium");
+    }
+
+
+   
 }
